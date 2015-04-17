aab248ea99bc3e066f8bb7f1ee972e4a9eeb78c7
Eric Magnuson <edm5gb@virginia.edu>
2015-04-17 14:05:05 -0400

    more bad changes

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 w = raw_input( 'enter weight in lbs: ' )
-h = raw_input( 'enter height in inches: bull ' )
+h = raw_input( 'enter height in inches: biggest ' )
 
 bmi = ( float(w) / float(h)**2 ) * 703
 print bmi
